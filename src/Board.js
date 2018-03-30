654c018a81537854313e086353afda217bdf3306
Muhammad Saad Mateen <saad@inspirat.us>
2018-03-31 03:53:50 +0400

    Fetch data dynamically with API to create notes on random locations

--- src/Board.js
+++ src/Board.js
@@ -1,28 +1,60 @@
 import React, {Component} from 'react'
 import Note from './Note'
+import FaPlus from 'react-icons/lib/fa/plus'
 
 class Board extends Component {
     constructor(props) {
         super(props)
         this.state = {
             notes: [
-                {
-                    id: 0,
-                    note: "Make breakfast"
-                },
-                {
-                    id: 1,
-                    note: "Email Ahsan"
-                },
-                {
-                    id: 2,
-                    note: "Learn React.js"
-                }
+                // {
+                //     id: 0,
+                //     note: "Make breakfast"
+                // },
+                // {
+                //     id: 1,
+                //     note: "Email Ahsan"
+                // },
+                // {
+                //     id: 2,
+                //     note: "Learn React.js"
+                // }
             ]
         }
+        this.add = this.add.bind(this)
         this.eachNote = this.eachNote.bind(this)
         this.update = this.update.bind(this)
         this.remove = this.remove.bind(this)
+        this.nextId = this.nextId.bind(this)
+    }
+    componentWillMount(){
+        var self = this
+        if(this.props.count) {
+            fetch(`https://baconipsum.com/api/?type=all-meat&sentences=${this.props.count}`)
+            .then(response => response.json())
+            .then(json => json[0]
+                                .split('. ')
+                                .forEach( 
+                                    sentence => self.add(sentence.substring(0,25) )
+                                )
+                    )
+        }
+    }
+
+    add(text) {
+        this.setState(prevState => ({
+            notes: [
+                ...prevState.notes,
+                {
+                    id: this.nextId(),
+                    note: text
+                }
+            ]
+        }) )
+    }
+    nextId() {
+        this.uniqueId = this.uniqueId || 0
+        return this.uniqueId++
     }
     update(newText, i) {
         console.log("updating item at index", i, newText)
@@ -40,7 +72,7 @@ class Board extends Component {
     }
     eachNote(note, i) {
         return (
-            <Note key={i} index={i} onChange={this.update} onRemove={this.remove}>
+            <Note key={note.id} index={note.id} onChange={this.update} onRemove={this.remove}>
                 {note.note}
             </Note>
         )
@@ -49,6 +81,7 @@ class Board extends Component {
         return(
             <div className="board">
                 {this.state.notes.map(this.eachNote)}
+                <button onClick={this.add.bind(null, "New Note")} id="add"><FaPlus/></button>
             </div>
         )
     }
